415e30d072b9586ab316c7008b97babffdc8d9bd
ProtyushB <protyushprincebanerjee@gmail.com>
2022-05-22 10:54:35 +0530

    Day5

---
+++ Day5_Question1/src/Employee.java
@@ -0,0 +1,43 @@
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private double salary;
+    private double netSalary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getNetSalary() {
+        return netSalary;
+    }
+
+    public void setNetSalary(double netSalary) {
+        this.netSalary = netSalary;
+    }
+
+    public void calculateNetSalary(int pfpercentage){
+
+        setNetSalary(this.getSalary()-(this.getSalary()*pfpercentage/100));
+    }
+}
